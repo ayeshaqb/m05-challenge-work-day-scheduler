384922097da0b6fbc796ef43904444a58dee17cd
ayeshaqb <ayesha.bajwa@gmail.com>
2022-08-29 19:39:09 -0500

    save user input to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
 //Display current day at top of calendar
 var currentDay = moment();
-$("#currentDay").text(currentDay.format("dddd, MMMM Do, YYYY"));
+$('#currentDay').text(currentDay.format(
+  'dddd, MMMM Do, YYYY'));
+
+var saveBtnEl = $('.saveBtn');
+
+//When DOM is fully loaded, execute the following functions...
+$(document).ready(function () {
+
+  //Save items to local storage when user clicks save button:
+  //When save button is clicked...
+  saveBtnEl.on('click', function () {
+    //...store the hour and text input into variables...
+    var hourKey = $(this).parent().attr('id');
+    var textValue = $(this).siblings('.description').val();
+    //...save this data to local storage.
+    localStorage.setItem(hourKey, textValue);
+  });
+
+});
+
+
