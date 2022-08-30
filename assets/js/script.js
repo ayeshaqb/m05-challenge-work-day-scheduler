f9a6554ef108ee1b0b0d5a511bb85d73210e4e46
ayeshaqb <ayesha.bajwa@gmail.com>
2022-08-29 19:58:33 -0500

    load any stored values from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ $('#currentDay').text(currentDay.format(
   'dddd, MMMM Do, YYYY'));
 
 var saveBtnEl = $('.saveBtn');
+var currentTime = moment().hours();
+console.log(currentTime);
 
 //When DOM is fully loaded, execute the following functions...
 $(document).ready(function () {
@@ -17,6 +19,20 @@ $(document).ready(function () {
     //...save this data to local storage.
     localStorage.setItem(hourKey, textValue);
   });
+
+
+  //return any stored values (text input) from local storage -- pass each key (time-block id). 
+  $('#09-am .description').val(localStorage.getItem('09-am'));
+  $('#10-am .description').val(localStorage.getItem('10-am'));
+  $('#11-am .description').val(localStorage.getItem('11-am'));
+  $('#12-pm .description').val(localStorage.getItem('12-pm'));
+  $('#01-pm .description').val(localStorage.getItem('01-pm'));
+  $('#02-pm .description').val(localStorage.getItem('02-pm'));
+  $('#03-pm .description').val(localStorage.getItem('03-pm'));
+  $('#04-pm .description').val(localStorage.getItem('04-pm'));
+  $('#05-pm .description').val(localStorage.getItem('05-pm'));
+
+
 
 });
 
